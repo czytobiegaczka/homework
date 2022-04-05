77701b31d5977792c9264efdcc06da7c9e8b7955
czytobiegaczka <czytobiegaczka@gmail.com>
2022-04-05 14:55:56 +0200

    synchronizacja z pracy

--- Ex_1_3_choinka.py
+++ Ex_1_3_choinka.py
@@ -2,8 +2,28 @@
 Zadanie 1.3 Choinka
 Napisz program, który wczytuje liczbę całkowitą, a następnie na konsolę wypisuje w tylu liniach
 „choinkę” ze znaków * . Np. dla parametru 3 powinno się wypisać:
-*
-***
-*****
+   *
+  ***
+ *****
+*******
 '''
 
+print('CHOINKA')
+print('```````\n')
+
+lines = 0
+
+while lines <= 0:
+    try:
+        lines = int(input('ilość poziomów: '))
+        if lines <= 0:
+            print('dopuszczalne tylko liczby większe od 0')
+    except Exception:
+        print('możliwe tylko liczby całkowite dodatnie')
+
+tree = ''
+napis = [lines]
+for count in range(1, lines + 1):
+    tree = (lines - count) * ' '
+    tree += ((count * 2) - 1) * '*'
+    print(tree)

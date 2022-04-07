24525577b23a0b278f4002df0c8295c70d34493d
czytobiegaczka <czytobiegaczka@gmail.com>
2022-04-07 14:54:27 +0200

    synchronizacja z pracy 07.04.2022  ruch gracza po planszy

--- Ex_1_6_skarb.py
+++ Ex_1_6_skarb.py
@@ -14,3 +14,53 @@ Dodatkowe utrudnienia, gdy zacznie działać pierwsza wersja:
 początku, umieść skarb w nowym miejscu (i od nowa zacznij liczyć kroki).
 • Z prawdopodobieństwem 1/5 nie podawaj graczowi wskazówki po wykonaniu kroku.
 '''
+
+from random import randint
+
+
+def zmiana_pozycji(pozycja, ruszyl):
+    print(pozycja, ruszyl)
+    a, b = pozycja
+    c, d = ruszyl
+    nowa = (a + c, b + d)
+    return nowa
+
+print('SKARB')
+print('~~~~~\n')
+
+pozycja_skarbu = (randint(1, 10), randint(1, 10))
+pozycja_gracza = (randint(1, 10), randint(1, 10))
+
+ruchy = {
+    'W': (0, 1),
+    'S': (0, -1),
+    'A': (-1, 0),
+    'D': (1, 0),
+}
+
+ilosc_prob = 0
+
+print(pozycja_skarbu)
+print(pozycja_gracza)
+
+aktualna_pozycja = pozycja_gracza
+
+print('Zgadnij gdzie jest skarb ?')
+print('W - góra \nS - dół \nD - prawo \nA - lewo')
+print(f'twoja pozycja: {pozycja_gracza}')
+
+while True:
+    ruch = (input('twój ruch: ')).upper()
+    if ruch in ruchy:
+        ilosc_prob += 1
+        nowa_pozycja = zmiana_pozycji(aktualna_pozycja, ruchy[ruch])
+        print(nowa_pozycja)
+        if nowa_pozycja == pozycja_skarbu:
+            print(f'ZWYCIĘSTWO\n dodatłeś do skarbu w {ilosc_prob} krokach')
+            break
+        aktualna_pozycja = nowa_pozycja
+
+    elif ruch == 'Q':
+        break
+    else:
+        print('to nie jest prawidłowy ruch')
